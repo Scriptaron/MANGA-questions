5f6be85c70b60fa6353e1e2a478080ddbb882f44
Scriptaron <aaron.manzone@gmail.com>
2024-06-13 23:21:43 -0300

    Adicionando o exercicio Valid e Merge list

--- easy/interview.py
+++ easy/interview.py
@@ -13,3 +13,19 @@ def two_sum(nums: list, target: int) -> list:
         diff = target - i
         if diff in nums:
             return [nums.index(i), nums.index(diff)]
+
+
+## Valid Parentheses
+
+# O(n²)
+def isValid_parentheses(s: str) -> bool:
+    while '[]' in s or '{}' in s or '()' in s:
+        s = s.replace('()', '')
+        s = s.replace('[]', '')
+        s = s.replace('{}', '')
+    return not s
+    
+## Merge Two Sorted Lists
+
+# O(1)
+merge_two_sorted_lists = lambda list1, list2 : sorted(list1+list2)
